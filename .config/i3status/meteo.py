0c07151d8e24da6cd984e8f10f613b81b66b3b66
LuisGMM <emaildemiscuentas@gmail.com>
2023-03-27 13:37:19 +0200

    .

--- .config/i3status/meteo.py
+++ .config/i3status/meteo.py
@@ -4,10 +4,10 @@ import json
 import urllib.request
 
 # sign in at https://openweathermap.org/api to get an API KEY
-APPID = "xxx"
+APPID = "1f07fb43029e9159ffd94a005104fdff"
 
 # get you city ID at http://bulk.openweathermap.org/sample/
-CITYID = "2968815"
+CITYID = "6359304"
 
 # {
 #     "coord": {
@@ -53,21 +53,24 @@ CITYID = "2968815"
 #     "cod": 200
 # }
 with urllib.request.urlopen("https://api.openweathermap.org/data/2.5/weather?id=" + CITYID + "&units=metric&appid=" + APPID) as url:
-  data = json.loads(url.read().decode())
-  str = " " + str(data['main']['temp']) + "°C "
-  # https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
-  if int(data['weather'][0]['id']) >= 200 and int(data['weather'][0]['id']) < 300: # Group 2xx: Thunderstorm
-    str += "" # yes, it's a thunder poo ;)
-  elif int(data['weather'][0]['id']) >= 300 and int(data['weather'][0]['id']) < 500: # Group 3xx: Drizzle
-    str += ""
-  elif int(data['weather'][0]['id']) >= 500 and int(data['weather'][0]['id']) < 600: # Group 5xx: Rain
-    str += ""
-  elif int(data['weather'][0]['id']) >= 600 and int(data['weather'][0]['id']) < 700: # Group 6xx: Snow
-    str += ""
-  elif int(data['weather'][0]['id']) >= 700 and int(data['weather'][0]['id']) < 800: # Group 7xx: Atmosphere
-    str += ""
-  elif int(data['weather'][0]['id']) == 800: # Group 800: Clear
-    str += ""
-  elif int(data['weather'][0]['id']) > 800: # Group 80x: Clouds
-    str += ""
-  print(str, end='')
+    data = json.loads(url.read().decode())
+    str = " " + str(data['main']['temp']) + "°C "
+    # https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
+
+    # Group 2xx: Thunderstorm
+    if int(data['weather'][0]['id']) >= 200 and int(data['weather'][0]['id']) < 300:
+        str += ""  # yes, it's a thunder poo ;)
+    elif int(data['weather'][0]['id']) >= 300 and int(data['weather'][0]['id']) < 500:  # Group 3xx: Drizzle
+        str += ""
+    elif int(data['weather'][0]['id']) >= 500 and int(data['weather'][0]['id']) < 600:  # Group 5xx: Rain
+        str += ""
+    elif int(data['weather'][0]['id']) >= 600 and int(data['weather'][0]['id']) < 700:  # Group 6xx: Snow
+        str += ""
+    # Group 7xx: Atmosphere
+    elif int(data['weather'][0]['id']) >= 700 and int(data['weather'][0]['id']) < 800:
+        str += ""
+    elif int(data['weather'][0]['id']) == 800:  # Group 800: Clear
+        str += ""
+    elif int(data['weather'][0]['id']) > 800:  # Group 80x: Clouds
+        str += ""
+    print(str, end='')
