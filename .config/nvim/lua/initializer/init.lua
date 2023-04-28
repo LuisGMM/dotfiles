d9da6b67e45b461351c443a958226775e1312edb
LuisGMM <69197163+LuisGMM@users.noreply.github.com>
2023-04-28 08:11:14 +0200

    .

--- .config/nvim/lua/initializer/init.lua
+++ .config/nvim/lua/initializer/init.lua
@@ -1,6 +1,7 @@
 require("initializer.packer")
 require("initializer.generic_remaps")
 require("initializer.vim_set")
+require("initializer.colorscheme")
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
