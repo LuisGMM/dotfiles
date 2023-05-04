f8e690166224a290aae2a6325bd8e07c3b880e12
LuisGMM <69197163+LuisGMM@users.noreply.github.com>
2023-05-04 14:34:34 +0200

    Update doge.lua

--- .config/nvim/after/plugin/doge.lua
+++ .config/nvim/after/plugin/doge.lua
@@ -1,5 +1,11 @@
 -- Doge setting to build numpy doc by default
 vim.g.doge_doc_standard_python = "numpy"
+
+vim.g.doge_python_settings = {
+	single_quotes = 0,
+	omit_redundant_param_types = 0,
+}
+
 --
 
 -- Keymap to autogenerate docstring
