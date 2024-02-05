4d84545d27d5df15b9528348b9f2a90f3d82a98a
LuisGMM <emaildemiscuentas@gmail.com>
2024-02-05 17:53:28 +0100

    Update flutter_tools.lua

--- .config/nvim/after/plugin/flutter_tools.lua
+++ .config/nvim/after/plugin/flutter_tools.lua
@@ -1,6 +1,14 @@
 require("lspconfig").dartls.setup({
 	cmd = { "dart", "language-server", "--protocol=lsp" },
 	lsp = {
+		color = { -- show the derived colours for dart variables
+			enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+			background = true, -- highlight the background
+			background_color = nil, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
+			foreground = true, -- highlight the foreground
+			virtual_text = true, -- show the highlight using virtual text
+			virtual_text_str = "■", -- the virtual text character to highlight
+		},
 		settings = {
 			enableSnippets = true,
 			updateImportsOnRename = true, -- Whether to update imports and other directives when files are renamed. Required for `FlutterRename` command.
