0fc5486c868117218f292d5efa8027fb21e171c3
LuisGMM <emaildemiscuentas@gmail.com>
2024-02-05 17:51:33 +0100

    Update python linters and fixers to mypy & ruff

--- .config/nvim/after/plugin/ale.lua
+++ .config/nvim/after/plugin/ale.lua
@@ -7,8 +7,9 @@ vim.g.ale_linters = {
 	},
 	python = {
 		"mypy",
-		"flake8",
-		"pycln",
+		"ruff",
+		-- "flake8",
+		-- "pycln",
 		-- "pydocstyle"
 	},
 	lua = {
@@ -27,18 +28,20 @@ vim.g.ale_fixers = {
 	},
 	python = {
 		"add_blank_lines_for_python_control_statements", -- Add blank lines before control statements.
-		"autoflake", -- Fix flake issues with autoflake.
+		-- "autoflake", -- Fix flake issues with autoflake.
 		-- "autoimport", -- Fix import issues with autoimport.
-		"autopep8", -- Fix PEP8 issues with autopep8.
+		-- "autopep8", -- Fix PEP8 issues with autopep8.
 		-- 'black', -- Fix PEP8 issues with black.
-		"isort", -- Sort Python imports with isort.
-		"pycln", -- remove unused python import statements
+		-- "isort", -- Sort Python imports with isort.
+		-- "pycln", -- remove unused python import statements
 		-- 'pyflyby', -- Tidy Python imports with pyflyby.
 		"remove_trailing_lines", -- Remove all blank lines at the end of a file.
 		-- 'reorder-python-imports', -- Sort Python imports with reorder-python-imports.
 		-- 'ruff', -- A python linter/fixer for Python written in Rust
 		"trim_whitespace", -- Remove all trailing whitespace characters at the end of every line.
 		-- 'yapf', -- Fix Python files with yapf.
+		"ruff",
+		"ruff_format",
 	},
 	lua = {
 		"stylua",
